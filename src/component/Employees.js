ed4c63b6649853ca85ea6ba5f7cabd10f05f675e
Deep5317 <deep.adak90@gmail.com>
2024-06-16 12:34:30 +0530

    Applying crud op using react

---
+++ src/component/Employees.js
@@ -0,0 +1,21 @@
+const Employees =[
+{
+    id:"1",
+    Name:"Deep",
+    Age:"20"
+},{
+    id:"2",
+    Name:"Dilip",
+    Age:"19"
+},{
+    id:"3",
+    Name:"Netram",
+    Age:"20"
+},{
+    id:"4",
+    Name:"Rudra",
+    Age:"21"
+}
+]
+
+export default Employees;
